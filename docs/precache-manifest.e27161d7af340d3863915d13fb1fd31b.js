c48136e91070552253f4012bfa437f2e67ff0d42
Clara Miralles <clara.miralles.imperial@gmail.com>
2019-05-19 15:39:30 +0200

    fix packake.json and built docsto publish in github pages

---
+++ docs/precache-manifest.e27161d7af340d3863915d13fb1fd31b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf39cab2f6938dc98ad87a87ee1ae095",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c333b2b7787c42efb8ef",
+    "url": "./static/css/main.9d270df6.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "./static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "c333b2b7787c42efb8ef",
+    "url": "./static/js/main.9f64afd8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
